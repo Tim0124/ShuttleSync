64eabab8f792c440ba84a06af454046d2576c29c
Tim Kao <a0925172969@gmail.com>
2024-11-12 22:58:24 +0800

    feat: setup swift to watch app

---
+++ ios/BadmintonScoreWatchApp Watch App/BadmintonScoreWatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BadmintonScoreWatchAppApp.swift
+//  BadmintonScoreWatchApp Watch App
+//
+//  Created by 高士程 on 2024/11/4.
+//
+
+import SwiftUI
+
+@main
+struct BadmintonScoreWatchApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios/BadmintonScoreWatchApp Watch AppTests/BadmintonScoreWatchApp_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  BadmintonScoreWatchApp_Watch_AppTests.swift
+//  BadmintonScoreWatchApp Watch AppTests
+//
+//  Created by 高士程 on 2024/11/4.
+//
+
+import Testing
+@testable import BadmintonScoreWatchApp_Watch_App
+
+struct BadmintonScoreWatchApp_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
